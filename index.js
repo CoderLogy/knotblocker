9e044f7de3fe0769ac662273d56b2c46424a0383
Lavender-Jamie :3 Null <lav@lavender.earth>
2025-03-15 13:02:28 -0700

    Kermit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // LOCK by Jamie
 adBlk = document.getElementById("ad-box");
-for (let i = 0; i < 5; i++) {
-    adBlk.innerHTML = '<div class="ad"></div>'
+for (let i = 0; i < 12; i++) {
+    adBlk.innerHTML += '<div class="ad"><img src=imgs/ad'+i+'.jpg style="max-width:100%; max-height:100%;"></div>'
 }
