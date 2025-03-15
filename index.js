b5e25b07b2dda0107ce7bdfbc6f92f96ddd402a9
Lavender-Jamie :3 Null <lav@lavender.earth>
2025-03-15 15:03:28 -0700

    Yayyyy done js lol

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // LOCK by Jamie
 adBlk = document.getElementById("ad-box");
-arr = ['ad0.png','ad1.png','ad2.jpg','ad3.webp','ad4.avif','ad5.png','ad6.jpg','ad7.png','ad8.png','ad9.webp','ad10.png','ad11.jpg']
+arr = ['ad1.png','ad2.jpg','ad3.webp','ad4.avif','ad5.png','ad6.jpg','ad7.png','ad8.png','ad9.webp','ad10.png','ad11.jpg']
 
 for (let i = 0; i < 12; i++) {
-    adBlk.innerHTML += '<div class="ad"><img src="imgs/'+ arr[i] +'" style="max-width:100%; max-height:100%;" alt="Advertisement"></div>'
+    adBlk.innerHTML += '<div id="ad'+i+'" onclick="resetAd('+i+')"><img src="imgs/'+ arr[i] +'" style="max-width:100%; max-height:100%;" alt="Advertisement"></div>'
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function resetAd(count) {
+    console.log(count)
+    adBlk2 = document.getElementById("ad"+count);
+    adBlk2.innerHTML = '<div id="ad'+count+'" onclick="resetAd('+count+')"><img src="imgs/'+ arr[getRandomInt(0,arr.length)] +'" style="max-width:100%; max-height:100%;" alt="Advertisement"></div>'
+}
+// UNLOCK by Jamie
