04d261097e5e1c1e37a1a66e13f891497a474b19
Sukhdeep Somal <117871204+TwistedMiraculous@users.noreply.github.com>
2025-03-15 16:21:40 -0700

    modified:   index.css 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // LOCK by Jamie
 adBlk = document.getElementById("ad-box");
-arr = ['ad1.png','ad2.jpg','ad3.webp','ad4.avif','ad5.png','ad6.jpg','ad7.png','ad8.png','ad9.webp','ad10.png','ad11.jpg']
+arr = ['ad1.png','ad2.jpg','ad3.webp','ad4.avif','ad5.png','ad6.jpg','ad7.png','ad8.png','ad9.webp','ad10.png','ad11.jpg'];
 
-for (let i = 0; i < 12; i++) {
-    adBlk.innerHTML += '<div id="ad'+i+'" onclick="resetAd('+i+')"><img src="imgs/'+ arr[i] +'" style="max-width:100%; max-height:100%;" alt="Advertisement"></div>'
+for (let i = 0; i < 10; i++) {
+     adBlk.innerHTML += '<div id="ad'+i+'" style="display: inline-block; padding: 0;"><img src="imgs/'+ arr[i] +'" onclick="resetAd('+i+')" style="max-width:300px; max-height:300px; display: block;" alt="Advertisement"></div>';
+     
 }
 
 function getRandomInt(min, max) {
@@ -12,9 +13,10 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function resetAd(count) {
-    console.log(count)
+async function resetAd(count) {
+    console.log(count);
     adBlk2 = document.getElementById("ad"+count);
-    adBlk2.innerHTML = '<img src="imgs/'+ arr[getRandomInt(0,arr.length-1)] +'" style="max-width:100%; max-height:100%;" alt="Advertisement">'
+    adBlk2.innerHTML = '<img src="imgs/'+ arr[getRandomInt(0, arr.length - 1)] +'" style="max-width:300px; max-height:300px;" alt="Advertisement">';
 }
 // UNLOCK by Jamie
+
