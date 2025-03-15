81398bec376c16d591a33c73ee85d8f5bcfd0126
Lavender-Jamie :3 Null <lav@lavender.earth>
2025-03-15 14:02:51 -0700

    kermitttt

--- index.js
+++ index.js
@@ -5,4 +5,3 @@ arr = ['ad0.png','ad1.png','ad2.jpg','ad3.webp','ad4.avif','ad5.png','ad6.jpg','
 for (let i = 0; i < 12; i++) {
     adBlk.innerHTML += '<div class="ad"><img src="imgs/'+ arr[i] +'" style="max-width:100%; max-height:100%;" alt="Advertisement"></div>'
 }
-
