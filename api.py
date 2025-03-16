b8f8f67878fd9f6bacbc4339c6177f0acb2986a4
Shourya wadhwa <71631493+CoderLogy@users.noreply.github.com>
2025-03-15 18:09:00 -0700

    d

--- api.py
+++ api.py
@@ -23,4 +23,4 @@ def generate_pun():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 if __name__ == '__main__':
-    app.run(debug=True)  # Set debug to False in production
+    app.run(debug=True,host=0.0.0.0)  # Set debug to False in production
